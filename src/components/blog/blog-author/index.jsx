ad810b6595be29671ab0142408ccaf80732c5d12
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-09-22 17:03:59 +0200

    Update index.jsx

--- src/components/blog/blog-author/index.jsx
+++ src/components/blog/blog-author/index.jsx
@@ -3,15 +3,17 @@ import { Row, Col, Image } from "react-bootstrap";
 import "./styles.css";
 export default class BlogAuthor extends Component {
   render() {
-    const { name, avatar } = this.props;
+    const {
+      author: { authorName, authorAvatar }
+    } = this.props;
     return (
       <Row>
         <Col xs={2}>
-          <Image className="blog-author" src={avatar} roundedCircle />
+          <Image className="blog-author" src={authorAvatar} roundedCircle />
         </Col>
         <Col>
           <div>by</div>
-          <h6>{name}</h6>
+          <h6>{authorName}</h6>
         </Col>
       </Row>
     );
