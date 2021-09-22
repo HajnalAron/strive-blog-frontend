41cb4e627adb2e45798482fc7dac73fdf09b3607
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-09-22 16:36:09 +0200

    Update config.js

--- src/Utilities/config.js
+++ src/Utilities/config.js
@@ -2,4 +2,5 @@ const isProduction = process.env.REACT_APP_ISPRODUCTION;
 const developmentBackend = process.env.REACT_APP_DEV_BACKEND_URL;
 const productionBackend = process.env.REACT_APP_PRODUCTION_BACKEND_URL;
 
-export const backendUrl = isProduction ? productionBackend : developmentBackend;
+export const backendUrl =
+  isProduction === "true" ? productionBackend : developmentBackend;
